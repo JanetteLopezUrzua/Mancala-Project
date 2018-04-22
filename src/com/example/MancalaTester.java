5298e37a6ba9beb1592ac92b1ac202fd9faf1e84
stivens73 <stivens54@gmail.com>
2018-04-22 14:59:49 -0700

    Initialized Tester class

--- src/com/example/MancalaTester.java
+++ src/com/example/MancalaTester.java
@@ -1,8 +1,18 @@
 package com.example;
 
+import com.example.Views.Board;
+import com.example.Views.RectangularStyle;
+
+import java.awt.*;
+
 public class MancalaTester {
 
-    public static void main(String[] args) {
+    public static final double BOARD_WIDTH = 800;
+    public static final double BOARD_HEIGHT = 300;
 
+    public static void main(String[] args) {
+//        Color color = new Color(Color.AQUA);
+
+        Board board = new Board(new RectangularStyle(Color.GRAY, BOARD_WIDTH, BOARD_HEIGHT));
     }
 }
--- src/com/example/Views/Board.java
+++ src/com/example/Views/Board.java
@@ -4,8 +4,18 @@ import java.awt.*;
 
 public class Board extends View {
 
+    AbstractGameStyle style;
+
+    public Board(AbstractGameStyle newStyle) {
+        style = newStyle;
+    }
+
     @Override
     public void draw(Graphics2D g2) {
 
+    }
+
+    public void initialize() {
+        //TODO: Initialize Mancalas and Pits
     }
 }
