519373ac9ec5bde2f71d2087fef71d56a3df37c5
stivens73 <stivens54@gmail.com>
2018-04-26 16:58:47 -0700

    BoardDragger

--- src/com/example/views/ShapedBoard.java
+++ src/com/example/views/ShapedBoard.java
@@ -3,10 +3,6 @@ package com.example.views;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import java.awt.geom.Ellipse2D;
-import java.awt.geom.RoundRectangle2D;
-
-import static java.awt.GraphicsDevice.WindowTranslucency.*;
 
 public class ShapedBoard extends JFrame {
 
@@ -22,23 +18,21 @@ public class ShapedBoard extends JFrame {
 
         setLayout(new BorderLayout());
 
-        // It is best practice to set the window's shape in
-        // the componentResized method.  Then, if the window
-        // changes size, the shape will be correctly recalculated.
         addComponentListener(new ComponentAdapter() {
-            // Give the window an elliptical shape.
-            // If the window is resized, the shape is recalculated here.
             @Override
             public void componentResized(ComponentEvent e) {
                 setShape(shape);
             }
         });
 
+
+        setTitle("Mancala");
         setUndecorated(true);
         setSize(width,height);
+
+        setResizable(true);
+//        set;
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        add(new JButton("I am a Button"));
     }
 }
